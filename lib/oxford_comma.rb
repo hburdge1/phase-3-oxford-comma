412132461446ab6a18bef28648a4ae737ac26881
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-01-31 12:02:35 -0800

    commit tree 696bf4074a44d52d79d29e2d71f4f0ddfd7415ca

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+new_arr=[]
+    if array.length <= 3 then
+    new_arr.push(array[0..-2])
+    new_arr.push([', and' , array[-1]])
+    return new_arr.join(" ")
+    elsif array.length == 1 then
+    return array
+    elsif array.length == 2 then
+    return 
+    
 end
